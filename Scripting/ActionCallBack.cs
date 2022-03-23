7c3b851e71cbd7d8c7e96ecb780c6f5b1e3ccc14
johnstennett24 <jstennett830@gmail.com>
2022-03-23 11:42:24 -0600

    scripting stuff

---
+++ Scripting/ActionCallBack.cs
@@ -0,0 +1,7 @@
+using System;
+using Raylib_cs;
+
+namespace HaloMiniGame.Scripting
+{
+    void OnNext(string SceneName);
+}
---
+++ Scripting/ControlActorsAction.cs
@@ -0,0 +1,13 @@
+using System;
+using Raylib_cs;
+
+namespace HaloMiniGame.Scripting
+{
+    public class ControlActorsAction : Action
+    {
+        private KeyBoardService keyBoardService;
+        private Point direction = new Point(constants.CELL_SIZE, 0);
+    }
+
+    
+}
