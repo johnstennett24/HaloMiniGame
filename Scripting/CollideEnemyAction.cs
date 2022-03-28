fe77a1b8672750917259ae4b7320a140a2006298
johnstennett24 <john.stennett@stencoinc.com>
2022-03-28 15:58:57 -0600

    added files to Scripting

---
+++ Scripting/CollideEnemyAction.cs
@@ -0,0 +1,32 @@
+using MiniHaloGame;
+
+
+namespace MiniHaloGame.Scripting
+{
+    public class CollideRacketAction : Action
+    {
+        private AudioService audioService;
+        private PhysicsService physicsService;
+        
+        public CollideRacketAction(PhysicsService physicsService, AudioService audioService)
+        {
+            this.physicsService = physicsService;
+            this.audioService = audioService;
+        }
+
+        public void Execute(Cast cast, Script script, ActionCallback callback)
+        {
+            Ball ball = (Ball)cast.GetFirstActor(Constants.PLAYER_GROUP);
+            Racket racket = (Racket)cast.GetFirstActor(Constants.ENEMY_GROUP);
+            Body ballBody = ball.GetBody();
+            Body racketBody = racket.GetBody();
+
+            if (physicsService.HasCollided(racketBody, ballBody))
+            {
+                ball.BounceY();
+                Sound sound = new Sound(Constants.BOUNCE_SOUND);
+                audioService.PlaySound(sound);
+            }
+        }
+    }
+}
