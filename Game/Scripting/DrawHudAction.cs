fce6706d666128d1af05189cba207442fd686967
johnstennett24 <jstennett830@gmail.com>
2022-03-29 17:04:24 -0600

    added the draw hud action class

---
+++ Game/Scripting/DrawHudAction.cs
@@ -0,0 +1,34 @@
+using HaloMiniGame.Game.Casting;
+using HaloMiniGame.Game.Directing;
+using HaloMiniGame.Game.Scripting;
+using HaloMiniGame.Game.Services;
+
+namespace HaloMiniGame.Game.Scripting
+{
+    public class DrawHudAction : Action
+    {
+        private VideoService videoService;
+        public DrawHudAction(VideoService videoService)
+        {
+            this.videoService = videoService;
+        }
+
+        public void Execute(Cast cast, Script script, ActionCallback callback)
+        {
+            Stats stats = (Stats)cast.GetFirstActor(Constants.STATS_GROUP);
+            DrawLabel(cast, Constants.LEVEL_GROUP, Constants.LEVEL_FORMAT, stats.GetLevel());
+            DrawLabel(cast, Constants.LIVES_GROUP, Constants.LIVES_FORMAT, stats.GetLives());
+        }
+
+        private void DrawLabel(Cast cast, string group, string format, int data)
+        {
+            Label label = (Label)cast.GetFirstActor(group);
+            Text text = label.GetText();
+            text.SetValue(string.Format(format, data));
+            Point position = label.GetPosition();
+            videoService.DrawText(text, position);
+        }
+
+        
+    }
+}
